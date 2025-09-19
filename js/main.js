0bcb782ca5047146a6aa115911df972a71182acb
MMohammed4321 <126342400+MMohammed4321@users.noreply.github.com>
2025-09-19 14:00:42 +0100

    Trying to fix mobile menu issue again and also adding a moving bg

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 const toggle = document.querySelector('.nav__toggle');
 const links = document.querySelector('.nav__links');
-if (toggle && links){
+const overlay = document.querySelector('.nav__overlay');
+
+if (toggle && links && overlay){
   toggle.addEventListener('click', ()=>{
     const open = links.classList.toggle('is-open');
+    overlay.classList.toggle('is-open', open);
     toggle.setAttribute('aria-expanded', String(open));
   });
+
+  overlay.addEventListener('click', ()=>{
+    links.classList.remove('is-open');
+    overlay.classList.remove('is-open');
+    toggle.setAttribute('aria-expanded','false');
+  });
 }
+
 document.getElementById('year').textContent = new Date().getFullYear();
 
 document.querySelectorAll('.nav__links a').forEach(link => {
   link.addEventListener('click', () => {
     links.classList.remove('is-open');
+    overlay.classList.remove('is-open');
     toggle.setAttribute('aria-expanded', 'false');
   });
 });
